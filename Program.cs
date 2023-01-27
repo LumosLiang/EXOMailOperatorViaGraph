33062f4a8f948238921bbf9629b9a1e8250b88eb
Administrator <Administrator@DAWANZI>
2023-01-27 16:31:44 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleMailSender;
+
+Console.WriteLine("Start Sending Email!");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+GraphHelper.InitializeGraphForAppOnlyAuth(settings);
+
+var sender = new SimpleEmailSender();
+int count = 0;
+
+while (true)
+{
+    await sender.SendMailAsync();
+    Console.WriteLine("Sending email {0}", count++);
+
+    if (count > 100)
+        break;
+
+    Thread.Sleep(10000);
+}
+
